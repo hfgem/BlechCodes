More snuck in bug fixes + held units code
Bug fixes dealing with running the BlechCodes pipeline on test-day datasets.
- functions/dev_plot_funcs.py: removed unnecessary calls, updated bins to match x-vals, removed diff plots
- functions/deviation_correlations.py: removed unnecessary call
- functions/null_deviation_correlations.py: updated folder creation
- utils/test_support.py: edits for testing changes

functions/dev_plot_funcs.py
@@ -908,7 +908,7 @@ def plot_dev_x_null_single_dist(null_data, true_val, plot_name, save_dir):
 
 
 def best_corr_calc_plot(dig_in_names, epochs_to_analyze, segments_to_analyze,
-                        segment_names, segment_deviations, segment_dev_times, dev_dir,
+                        segment_names, segment_dev_times, dev_dir,
                         corr_data_dir, save_dir):
     """This function calculates which deviation is correlated most to which
     condition and plots"""
@@ -1018,7 +1018,7 @@ def best_corr_calc_plot(dig_in_names, epochs_to_analyze, segments_to_analyze,
 
     # Plot the cumulative distribution functions by segment and the density differences
     density_x_vals = np.arange(0, 1.1, 0.025)
-    density_bins = np.arange(-0.05, 1.15, 0.025)
+    density_bins = np.concatenate((density_x_vals - 0.05,1.125*np.ones(1)))
     taste_pairs = list(combinations(np.arange(num_tastes), 2))
     epoch_pairs = list(combinations(np.arange(len(epochs_to_analyze)), 2))
 
@@ -1026,14 +1026,6 @@ def best_corr_calc_plot(dig_in_names, epochs_to_analyze, segments_to_analyze,
         nrows=len(segments_to_analyze), ncols=2, figsize=(8, 8))
     f_dens, ax_dens = plt.subplots(
         nrows=len(segments_to_analyze), ncols=2, figsize=(8, 8))
-    f_diff_taste, ax_diff_taste = plt.subplots(
-        nrows=1, ncols=len(taste_pairs), figsize=(15, 5))
-    max_taste_diff = 0
-    min_taste_diff = 0
-    f_diff_epoch, ax_diff_epoch = plt.subplots(
-        nrows=1, ncols=len(epoch_pairs), figsize=(15, 5))
-    max_epoch_diff = 0
-    min_epoch_diff = 0
     for s_ind, s_i in enumerate(segments_to_analyze):  # By rest interval
         dev_sorting = seg_data[s_i]
         # Taste data
@@ -1055,23 +1047,6 @@ def best_corr_calc_plot(dig_in_names, epochs_to_analyze, segments_to_analyze,
                 density_x_vals, density_hist_data[0], color=taste_colors[t_i, :], label=dig_in_names[t_i])
             ax_dens[s_ind, 0].set_ylim([-0.05, 1.05])
             ax_dens[s_ind, 0].set_xlim([-0.05, 1.05])
-        for tp_ind, t_pair in enumerate(taste_pairs):
-            t_1 = t_pair[1]
-            t_2 = t_pair[0]
-            t_1_name = dig_in_names[t_1]
-            t_2_name = dig_in_names[t_2]
-            hist_diff = taste_density[t_2] - taste_density[t_1]
-            if s_ind == 0:
-                ax_diff_taste[tp_ind].set_title(t_2_name + ' - ' + t_1_name)
-                ax_diff_taste[tp_ind].set_xlabel('Correlation')
-                if tp_ind == 0:
-                    ax_diff_taste[tp_ind].set_ylabel('Density Difference')
-            if max(hist_diff) > max_taste_diff:
-                max_taste_diff = max(hist_diff)
-            if min(hist_diff) < min_taste_diff:
-                min_taste_diff = min(hist_diff)
-            ax_diff_taste[tp_ind].plot(
-                density_x_vals, hist_diff, label=segment_names[s_i], color=segment_colors[s_ind, :])
         # Epoch data
         epoch_density = []
         for e_ind, e_i in enumerate(epochs_to_analyze):
@@ -1091,23 +1066,6 @@ def best_corr_calc_plot(dig_in_names, epochs_to_analyze, segments_to_analyze,
                 density_x_vals, density_hist_data[0], color=epoch_colors[e_ind, :], label='Epoch ' + str(e_i))
             ax_dens[s_ind, 1].set_ylim([-0.05, 1.05])
             ax_dens[s_ind, 1].set_xlim([-0.05, 1.05])
-        for ep_ind, e_pair in enumerate(epoch_pairs):
-            e_1 = e_pair[0]
-            e_2 = e_pair[1]
-            e_1_name = 'Epoch ' + str(e_1)
-            e_2_name = 'Epoch ' + str(e_2)
-            hist_diff = epoch_density[e_2] - epoch_density[e_1]
-            if s_ind == 0:
-                ax_diff_epoch[ep_ind].set_title(e_2_name + ' - ' + e_1_name)
-                ax_diff_epoch[ep_ind].set_xlabel('Correlation')
-                if ep_ind == 0:
-                    ax_diff_epoch[ep_ind].set_ylabel('Density Difference')
-            if max(hist_diff) > max_epoch_diff:
-                max_epoch_diff = max(hist_diff)
-            if min(hist_diff) < min_epoch_diff:
-                min_epoch_diff = min(hist_diff)
-            ax_diff_epoch[ep_ind].plot(
-                density_x_vals, hist_diff, label=segment_names[s_i], color=segment_colors[s_ind, :])
         # Plot cleanups
         ax_cum[s_ind, 0].set_ylabel(segment_names[s_i])
         ax_dens[s_ind, 0].set_ylabel(segment_names[s_i])
@@ -1135,37 +1093,6 @@ def best_corr_calc_plot(dig_in_names, epochs_to_analyze, segments_to_analyze,
     f_dens.savefig(os.path.join(save_dir, 'best_corr_norm_dens_hist.png'))
     f_dens.savefig(os.path.join(save_dir, 'best_corr_norm_dens_hist.svg'))
     plt.close(f_dens)
-    # Taste diff plot cleanups
-    ax_diff_taste[0].legend(loc='upper left')
-    for tp_ind in range(len(taste_pairs)):
-        ax_diff_taste[tp_ind].axhline(
-            0, alpha=0.5, color='k', linestyle='dashed', label='_')
-        ax_diff_taste[tp_ind].axvline(
-            0.5, alpha=0.5, color='k', linestyle='dashed', label='_')
-        ax_diff_taste[tp_ind].set_xlim([-0.05, 1.05])
-        ax_diff_taste[tp_ind].set_ylim(
-            [min_taste_diff + 0.1*min_taste_diff, max_taste_diff + 0.1*max_taste_diff])
-    f_diff_taste.suptitle('Taste Density Differences of Best Events')
-    plt.tight_layout()
-    f_diff_taste.savefig(os.path.join(save_dir, 'best_corr_taste_diff.png'))
-    f_diff_taste.savefig(os.path.join(save_dir, 'best_corr_taste_diff.svg'))
-    plt.close(f_diff_taste)
-    # Epoch diff plot cleanups
-    ax_diff_epoch[0].legend(loc='upper left')
-    for ep_ind in range(len(epoch_pairs)):
-        ax_diff_epoch[ep_ind].axhline(
-            0, alpha=0.5, color='k', linestyle='dashed', label='_')
-        ax_diff_epoch[ep_ind].axvline(
-            0.5, alpha=0.5, color='k', linestyle='dashed', label='_')
-        ax_diff_epoch[ep_ind].set_xlim([-0.05, 1.05])
-        ax_diff_epoch[ep_ind].set_ylim(
-            [min_epoch_diff + 0.1*min_epoch_diff, max_epoch_diff + 0.1*max_epoch_diff])
-    f_diff_epoch.suptitle('Epoch Density Differences of Best Events')
-    plt.tight_layout()
-    f_diff_epoch.savefig(os.path.join(save_dir, 'best_corr_epoch_diff.png'))
-    f_diff_epoch.savefig(os.path.join(save_dir, 'best_corr_epoch_diff.svg'))
-    plt.close(f_diff_epoch)
-    
     
 def sig_count_plot(sig_dev_counts, segments_to_analyze, segment_names,
                    dig_in_names, save_dir):

utils/test_support.py
@@ -63,62 +63,116 @@ tastant_spike_times = af.calc_tastant_spike_times(data_dict['segment_times'],dat
 data_dict['segment_spike_times'] = segment_spike_times
 data_dict['tastant_spike_times'] = tastant_spike_times
 
-#%% Changepoint Support
+#%% Dev Corr Support
 
-import functions.analysis_funcs as af
+import os
+import json
+import gzip
+import tqdm
+import numpy as np
+import functions.dev_plot_funcs as dpf
+import functions.dev_funcs as df
 import functions.hdf5_handling as hf5
-import functions.changepoint_detection as cd
-import functions.plot_funcs as pf
-import functions.decoding_funcs as df
 
-cp_bin = metadata['params_dict']['cp_bin']
-num_cp = metadata['params_dict']['num_cp']
-before_taste = np.ceil(metadata['params_dict']['pre_taste']*1000).astype('int') #Milliseconds before taste delivery to plot
-after_taste = np.ceil(metadata['params_dict']['post_taste']*1000).astype('int') #Milliseconds after taste delivery to plot
+dev_dir = metadata['dir_name'] + 'Deviations/'
 hdf5_dir = metadata['hdf5_dir']
+comp_dir = metadata['dir_name'] + 'dev_x_taste/'
+if os.path.isdir(comp_dir) == False:
+    os.mkdir(comp_dir)
+corr_dir = comp_dir + 'corr/'
+if os.path.isdir(corr_dir) == False:
+    os.mkdir(corr_dir)
+# Params/Variables
+num_neur = data_dict['num_neur']
+pre_taste = metadata['params_dict']['pre_taste']
+post_taste = metadata['params_dict']['post_taste']
+segments_to_analyze = metadata['params_dict']['segments_to_analyze']
+epochs_to_analyze = metadata['params_dict']['epochs_to_analyze']
+segment_names = data_dict['segment_names']
+num_segments = len(segment_names)
+segment_spike_times = data_dict['segment_spike_times']
+segment_times = data_dict['segment_times']
+segment_times_reshaped = [
+    [segment_times[i], segment_times[i+1]] for i in range(num_segments)]
+# Remember this is 1 less than the number of epochs
+num_cp = metadata['params_dict']['num_cp']
+tastant_spike_times = data_dict['tastant_spike_times']
 start_dig_in_times = data_dict['start_dig_in_times']
 end_dig_in_times = data_dict['end_dig_in_times']
 dig_in_names = data_dict['dig_in_names']
+z_bin = metadata['params_dict']['z_bin']
 
+print("\tNow importing calculated deviations")
+segment_deviations = []
+for s_i in tqdm.tqdm(segments_to_analyze):
+    filepath = dev_dir + \
+        segment_names[s_i] + '/deviations.json'
+    with gzip.GzipFile(filepath, mode="r") as f:
+        json_bytes = f.read()
+        json_str = json_bytes.decode('utf-8')
+        data = json.loads(json_str)
+        segment_deviations.append(data)
+print("\tNow pulling true deviation rasters")
+num_segments = len(segments_to_analyze)
+segment_spike_times_reshaped = [segment_spike_times[i]
+                       for i in segments_to_analyze]
+segment_times_reshaped = np.array(
+    [segment_times_reshaped[i] for i in segments_to_analyze])
+segment_dev_rasters, segment_dev_times, segment_dev_vec, segment_dev_vec_zscore = df.create_dev_rasters(num_segments,
+                                                                                                        segment_spike_times_reshaped,
+                                                                                                        segment_times_reshaped,
+                                                                                                        segment_deviations, z_bin)
 
-#Set storage directory
-cp_save_dir =metadata['dir_name'] + 'Changepoint_Calculations/'
-if os.path.isdir(cp_save_dir) == False:
-	os.mkdir(cp_save_dir)
-
-#_____All data_____
-taste_cp_save_dir = cp_save_dir + 'All_Taste_CPs/'
-if os.path.isdir(taste_cp_save_dir) == False:
-	os.mkdir(taste_cp_save_dir)
 data_group_name = 'changepoint_data'
-#Raster Poisson Bayes Changepoint Calcs Indiv Neurons
-try:
-	taste_cp_raster_inds = hf5.pull_data_from_hdf5(hdf5_dir,data_group_name,'taste_cp_raster_inds')
-	pop_taste_cp_raster_inds = hf5.pull_data_from_hdf5(hdf5_dir,data_group_name,'pop_taste_cp_raster_inds')
-except:	
-	taste_cp_raster_save_dir = taste_cp_save_dir + 'neur/'
-	if os.path.isdir(taste_cp_raster_save_dir) == False:
-		os.mkdir(taste_cp_raster_save_dir)
-	taste_cp_raster_inds = cd.calc_cp_iter(tastant_spike_times,cp_bin,num_cp,start_dig_in_times,
-				  end_dig_in_times,before_taste,after_taste,
-				  dig_in_names,taste_cp_raster_save_dir)
-	hf5.add_data_to_hdf5(hdf5_dir,data_group_name,'taste_cp_raster_inds',taste_cp_raster_inds)
-	
-	taste_cp_raster_pop_save_dir = taste_cp_save_dir + 'pop/'
-	if os.path.isdir(taste_cp_raster_pop_save_dir) == False:
-		os.mkdir(taste_cp_raster_pop_save_dir)
-	pop_taste_cp_raster_inds = cd.calc_cp_iter_pop(tastant_spike_times,cp_bin,num_cp,start_dig_in_times,
-				  end_dig_in_times,before_taste,after_taste,
-				  dig_in_names,taste_cp_raster_pop_save_dir)
-	hf5.add_data_to_hdf5(hdf5_dir,data_group_name,'pop_taste_cp_raster_inds',pop_taste_cp_raster_inds)
+pop_taste_cp_raster_inds = hf5.pull_data_from_hdf5(
+    hdf5_dir, data_group_name, 'pop_taste_cp_raster_inds')
+pop_taste_cp_raster_inds = pop_taste_cp_raster_inds
+num_pt_cp = num_cp + 2
+# Import discriminability data
+data_group_name = 'taste_discriminability'
+peak_epochs = np.squeeze(hf5.pull_data_from_hdf5(
+    hdf5_dir, data_group_name, 'peak_epochs'))
+discrim_neur = np.squeeze(hf5.pull_data_from_hdf5(
+    hdf5_dir, data_group_name, 'discrim_neur'))
+# Convert discriminatory neuron data into pop_taste_cp_raster_inds shape
+# TODO: Test this first, then if going with this rework functions to fit instead!
+num_discrim_cp = np.shape(discrim_neur)[0]
+discrim_cp_raster_inds = []
+for t_i in range(len(dig_in_names)):
+    t_cp_vec = np.ones(
+        (np.shape(pop_taste_cp_raster_inds[t_i])[0], num_discrim_cp))
+    t_cp_vec = (peak_epochs[:num_pt_cp] +
+                int(pre_taste*1000))*t_cp_vec
+    discrim_cp_raster_inds.append(t_cp_vec)
+num_discrim_cp = len(peak_epochs)
 
+current_corr_dir = corr_dir + 'all_neur/'
+if os.path.isdir(current_corr_dir) == False:
+    os.mkdir(current_corr_dir)
+#neuron_keep_indices = np.ones((num_neur,num_cp+1))
+neuron_keep_indices = np.ones(np.shape(discrim_neur))
+# Calculate correlations
+df.calculate_vec_correlations(num_neur, segment_dev_vec, tastant_spike_times,
+                              start_dig_in_times, end_dig_in_times, segment_names,
+                              dig_in_names, pre_taste, post_taste, pop_taste_cp_raster_inds,
+                              current_corr_dir, neuron_keep_indices, segments_to_analyze)  # For all neurons in dataset
+# Calculate significant events
+sig_dev, sig_dev_counts = df.calculate_significant_dev(segment_dev_times, 
+                                                       segment_times, dig_in_names,
+                                                       segment_names, current_corr_dir,
+                                                       segments_to_analyze)
 
+best_dir = current_corr_dir + 'best/'
+if os.path.isdir(best_dir) == False:
+    os.mkdir(best_dir)
+
+corr_data_dir = current_corr_dir
+save_dir = best_dir
 
 #%% CP Dist Plots
 
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.stats as stats
 
 colors = ['g','b','o','p','r']
 
@@ -136,9 +190,9 @@ for t_i in range(len(dig_in_names)):
     for cp_i in range(2):
         plt.hist(taste_cp_realigned[:,cp_i],density=True,alpha=0.3,label='Changepoint ' + str(cp_i+1),color=colors[cp_i])
         dist_mean = np.nanmean(taste_cp_realigned[:,cp_i])
-        dist_mode = stats.mode(taste_cp_realigned[:,cp_i])[0]
+        dist_median = np.median(taste_cp_realigned[:,cp_i])
         plt.axvline(dist_mean,label='Mean CP ' + str(cp_i+1) + ' = ' + str(np.round(dist_mean,2)), color=colors[cp_i])
-        plt.axvline(dist_mean,label='Mode CP ' + str(cp_i+1) + ' = ' + str(np.round(dist_mode,2)), linestyle='dashed', color=colors[cp_i])
+        plt.axvline(dist_median,label='Median CP ' + str(cp_i+1) + ' = ' + str(np.round(dist_median,2)), linestyle='dashed', color=colors[cp_i])
     plt.legend()
     plt.title(dig_in_names[t_i])
     plt.xlabel('Time Post Taste Delivery (ms)')