Saving and bug fix + held units
- functions/dev_sequence_funcs.py: added storing of both true and null sequence calculations for re-import later (save the re-calc time) + minor bug fix of percentile calculated with absolute value.
- detect_held_units.py: snuck in some edits of this script in calculating held units - testing if adding in firing information to calculations improves neuron selection.

detect_held_units.py
@@ -17,6 +17,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from functions.blech_held_units_funcs import *
     
+# Time to use after a taste is delivered to calculate an average firing rate for each neuron
+post_deliv_time = 500
+
 # Ask the user for the number of days to be compared
 num_days = int_input("How many days-worth of data are you comparing for held units (integer)? ")
 
@@ -51,31 +54,92 @@ for n_i in range(num_days):
     num_neur = len(hf5.root.unit_descriptor[:])
     all_neur_inds.append(list(np.arange(num_neur)))
     data_dict[n_i]['num_neur'] = num_neur
+    #Check if dig in times saved as csv in directory or not
+    start_dig_in_times_csv = os.path.join(dir_name,'start_dig_in_times.csv')
+    if os.path.isfile(start_dig_in_times_csv):
+        print("\t\tImporting previously saved digital input times")
+        start_dig_in_times = []
+        with open(start_dig_in_times_csv, 'r') as file:
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                start_dig_in_times.append(list(np.array(row).astype('int')))
+        num_tastes = len(start_dig_in_times)
+    else:
+        dig_in_node = blech_clust_h5.list_nodes('/digital_in')
+        dig_in_indices = np.array([d_i.name.split('_')[-1] for d_i in dig_in_node])
+        dig_in_ind = []
+        i = 0
+        for d_i in dig_in_indices:
+            try:
+                int(d_i)
+                dig_in_ind.extend([i])
+            except:
+                "not an input - do nothing"
+            i += 1
+        del dig_in_indices
+        try:
+            if len(dig_in_node[0][0]):
+                dig_in_data = [list(dig_in_node[d_i][0][:]) for d_i in dig_in_ind]
+        except:
+            dig_in_data = [list(dig_in_node[d_i][:]) for d_i in dig_in_ind]
+        num_tastes = len(dig_in_data)
+        del dig_in_node
+        #_____Convert dig_in_data to indices of dig_in start and end times_____
+        print("\tConverting digital inputs to free memory")
+        #Again, all are converted to ms timescale
+        start_dig_in_times = [list(np.ceil((np.where(np.diff(np.array(dig_in_data[i])) == 1)[0] + 1)*self.ms_conversion).astype('int')) for i in range(num_tastes)]    
+        #Store these into csv for import in future instead of full dig_in_data load which takes forever!
+        with open(start_dig_in_times_csv, 'w') as f:
+            write = csv.writer(f,delimiter=',')
+            write.writerows(start_dig_in_times)
+    flat_start_dig_in_times = []
+    for d_i in len(start_dig_in_times):
+        flat_start_dig_in_times.extend(start_dig_in_times[d_i])
+    start_dig_in_times = np.sort(np.array(flat_start_dig_in_times)) #All dig in times - regardless of taste
+    data_dict[n_i]['start_dig_in_times'] = start_dig_in_times
+    taste_start_time = np.max([np.min(start_dig_in_times) - 5000,0]).astype('int')
+    taste_end_time = (np.max(start_dig_in_times) + 5000).astype('int')
+    data_dict[n_i]['taste_interval'] = [taste_start_time,taste_end_time]
     #Calculate the Intra-J3 data for the units
     intra_J3 = []
+    all_unit_waveforms = []
+    all_unit_times = []
     for unit in range(num_neur):
         # Only go ahead if this is a single unit
         if hf5.root.unit_descriptor[unit]['single_unit'] == 1:
-            exec("wf_day1 = hf5.root.sorted_units.unit%03d.waveforms[:]" % (unit))
+            exec("wf_day1 = hf5.root.sorted_units.unit%03d.waveforms[:]" % (unit)) #num wav x 60
+            exec("t_day1 = hf5.root.sorted_units.unit%03d.times[:]" % (unit)) #num wav
             if wf_type == 'norm_waveform':
                 wf_day1 = wf_day1 / np.std(wf_day1)
+            #Pull out taste response interval waveforms only
+            taste_wf_inds = np.where((t_day1 >= taste_start_time)*(t_day1 <= taste_end_time))[0]
+            taste_t_day1 = t_day1[taste_wf_inds]
+            all_unit_times.append(taste_t_day1)
+            taste_wf_day1 = wf_day1[taste_wf_inds,:]
+            all_unit_waveforms.append(taste_wf_day1)
             pca = PCA(n_components = 4)
-            pca.fit(wf_day1)
-            pca_wf_day1 = pca.transform(wf_day1)
-            intra_J3.append(calculate_J3(pca_wf_day1[:int(wf_day1.shape[0]*(1.0/3.0)), :], pca_wf_day1[int(wf_day1.shape[0]*(2.0/3.0)):, :]))
+            pca.fit(taste_wf_day1)
+            pca_wf_day1 = pca.transform(taste_wf_day1) #num wav x 4
+            #Calculate firing rates within given interval following taste delivery
+            neur_fr = np.zeros(len(start_dig_in_times))
+            for st_i in start_dig_in_times:
+                spike_hz = len(np.where((taste_t_day1>st_i)*(taste_t_day<=st_i+post_deliv_time))[0])/(post_deliv_time/1000)
+                neur_fr[st_i] = spike_hz
+            mean_fr = np.nanmean(neur_fr)
+            std_fr = np.nanstd(neur_fr)
+            #Add to PCA of waveform the firing rate data in the form of mean and standard deviation
+            pca_wf_day1 = np.concatenate((pca_wf_day1,mean_fr*np.ones((len(taste_t_day1),0)),std_fr*np.ones((len(taste_t_day1),0))),axis=1)
+            intra_J3.append(calculate_J3(pca_wf_day1[:int(taste_wf_day1.shape[0]*(1.0/3.0)), :], 
+                                         pca_wf_day1[int(taste_wf_day1.shape[0]*(2.0/3.0)):, :]))
     data_dict[n_i]['intra_J3'] = intra_J3
+    data_dict[n_i]['all_unit_waveforms'] = all_unit_waveforms
+    data_dict[n_i]['all_unit_times'] = all_unit_times
     all_intra_J3.extend(intra_J3)
     #Pull unit info for all units
     all_unit_info = []
     for unit in range(num_neur):
         all_unit_info.append(get_unit_info(hf5.root.unit_descriptor[unit]))
     data_dict[n_i]['all_unit_info'] = all_unit_info
-    #Pull unit waveforms for all units
-    all_unit_waveforms = []
-    for unit in range(num_neur):
-        exec("wf = hf5.root.sorted_units.unit%03d.waveforms[:]" % (unit))
-        all_unit_waveforms.append(wf)
-    data_dict[n_i]['all_unit_waveforms'] = all_unit_waveforms
     #Close hdf5 file
     hf5.close()
     

functions/dev_sequence_funcs.py
@@ -274,66 +274,75 @@ def create_sequence_run_calcs(num_null, num_dev, seg_dev_rast, seg_z_mean,
                         s_i, bin_dt, epochs_to_analyze, tastant_raster_dict, 
                         taste_seqs_dict, avg_taste_seqs_dict, sequence_dir, 
                         null_sequence_dir, null_sequence_dir_2):
-    tic = time.time()
-    dev_seqs = []
-    null_dev_dict = dict()
-    null_dev_dict_2 = dict()
-    for null_i in range(num_null):
-        null_dev_dict[null_i] = []
-        null_dev_dict_2[null_i] = []
-        
-    #Calculate deviation sequences
-    for dev_i in range(num_dev):
-        #Pull raster and count up firing within bins
-        dev_rast = seg_dev_rast[dev_i]
-        num_spikes_per_neur = np.sum(dev_rast,1).astype('int')
-        _, num_dt = np.shape(dev_rast)
-        bin_starts = np.arange(num_dt-bin_dt)
-        dev_bin_counts = np.zeros((num_neur,len(bin_starts)))
-        for bs in bin_starts:
-            dev_bin_counts[:,bs] = np.sum(dev_rast[:,bs:bs+bin_dt],1)
-        max_counts = np.max(dev_bin_counts,1)
-        #Neuron Ordering: note that if multiple neurons fire the same number 
-        #of times, it will order them by index 
-        max_ind = calc_max_ind(num_neur, dev_bin_counts, max_counts)
-        neur_ord = np.argsort(max_ind, kind='stable')
-        dev_seqs.append(neur_ord)
-        
-        #Create null versions of the event
+    try: #Import of pre-calculated sequences
+        dev_seqs_array = np.load(os.path.join(sequence_dir,segment_names[s_i] + '_dev_seqs_array.npy'),allow_pickle=True)
+        null_dev_dict = np.load(os.path.join(null_sequence_dir,segment_names[s_i] + '_null_dev_dict.npy'),allow_pickle=True).item()
+        null_dev_dict_2 = np.load(os.path.join(null_sequence_dir_2,segment_names[s_i] + '_null_dev_dict.npy'),allow_pickle=True).item()
+        print("\t\t\t\t\tImported previously calculated sequences for " + segment_names[s_i])
+    except: #Create sequences
+        tic = time.time()
+        dev_seqs = []
+        null_dev_dict = dict()
+        null_dev_dict_2 = dict()
         for null_i in range(num_null):
-            #Shuffle within-neuron spike times
-            shuffle_rast = np.zeros(np.shape(dev_rast))
-            for neur_i in range(num_neur):
-                new_spike_ind = random.sample(list(np.arange(num_dt)),num_spikes_per_neur[neur_i])
-                shuffle_rast[neur_i,new_spike_ind] = 1
-            dev_bin_counts = np.zeros((num_neur,len(bin_starts)))
-            for bs in bin_starts:
-                dev_bin_counts[:,bs] = np.sum(shuffle_rast[:,bs:bs+bin_dt],1)
-            max_counts = np.max(dev_bin_counts,1)
-            max_ind = calc_max_ind(num_neur, dev_bin_counts, max_counts)
-            neur_ord = np.argsort(max_ind, kind='stable')
-            null_dev_dict[null_i].append(neur_ord)
-            #Shuffle across-neuron spike times
-            shuffle_rast_2 = np.zeros(np.shape(dev_rast))
-            new_neuron_order = random.sample(list(np.arange(num_neur)),num_neur)
-            for nn_ind, nn in enumerate(new_neuron_order):
-                shuffle_rast_2[nn_ind,:] = shuffle_rast[nn,:]
-            dev_bin_counts = np.zeros((num_neur,len(bin_starts)))
-            for bs in bin_starts:
-                dev_bin_counts[:,bs] = np.sum(shuffle_rast[:,bs:bs+bin_dt],1)
-            max_counts = np.max(dev_bin_counts,1)
-            max_ind = calc_max_ind(num_neur, dev_bin_counts, max_counts)
-            neur_ord = np.argsort(max_ind, kind='stable')
-            null_dev_dict_2[null_i].append(neur_ord)
+            null_dev_dict[null_i] = []
+            null_dev_dict_2[null_i] = []
             
-    dev_seqs_array = np.array(dev_seqs) #num dev x num neur
-    for null_i in range(num_null):
-        null_dev_dict[null_i] = np.array(null_dev_dict[null_i]) #num dev x num neur x 2
-        null_dev_dict_2[null_i] = np.array(null_dev_dict_2[null_i]) #num dev x num neur x 2
-    toc = time.time()
-    print('\t\t\t\t\tTime to calculate sequences for ' + segment_names[s_i] + \
-          ' = ' + str(np.round((toc-tic)/60, 2)) + ' (min)')
-    
+        #Calculate deviation sequences
+        for dev_i in range(num_dev):
+            #Pull raster and count up firing within bins
+            dev_rast = seg_dev_rast[dev_i]
+            num_spikes_per_neur = np.sum(dev_rast,1).astype('int')
+            _, num_dt = np.shape(dev_rast)
+            bin_starts = np.arange(num_dt-bin_dt)
+            dev_bin_counts = np.zeros((num_neur,len(bin_starts)))
+            for bs in bin_starts:
+                dev_bin_counts[:,bs] = np.sum(dev_rast[:,bs:bs+bin_dt],1)
+            max_counts = np.max(dev_bin_counts,1)
+            #Neuron Ordering: note that if multiple neurons fire the same number 
+            #of times, it will order them by index 
+            max_ind = calc_max_ind(num_neur, dev_bin_counts, max_counts)
+            neur_ord = np.argsort(max_ind, kind='stable')
+            dev_seqs.append(neur_ord)
+            
+            #Create null versions of the event
+            for null_i in range(num_null):
+                #Shuffle within-neuron spike times
+                shuffle_rast = np.zeros(np.shape(dev_rast))
+                for neur_i in range(num_neur):
+                    new_spike_ind = random.sample(list(np.arange(num_dt)),num_spikes_per_neur[neur_i])
+                    shuffle_rast[neur_i,new_spike_ind] = 1
+                dev_bin_counts = np.zeros((num_neur,len(bin_starts)))
+                for bs in bin_starts:
+                    dev_bin_counts[:,bs] = np.sum(shuffle_rast[:,bs:bs+bin_dt],1)
+                max_counts = np.max(dev_bin_counts,1)
+                max_ind = calc_max_ind(num_neur, dev_bin_counts, max_counts)
+                neur_ord = np.argsort(max_ind, kind='stable')
+                null_dev_dict[null_i].append(neur_ord)
+                #Shuffle across-neuron spike times
+                shuffle_rast_2 = np.zeros(np.shape(dev_rast))
+                new_neuron_order = random.sample(list(np.arange(num_neur)),num_neur)
+                for nn_ind, nn in enumerate(new_neuron_order):
+                    shuffle_rast_2[nn_ind,:] = shuffle_rast[nn,:]
+                dev_bin_counts = np.zeros((num_neur,len(bin_starts)))
+                for bs in bin_starts:
+                    dev_bin_counts[:,bs] = np.sum(shuffle_rast_2[:,bs:bs+bin_dt],1)
+                max_counts = np.max(dev_bin_counts,1)
+                max_ind = calc_max_ind(num_neur, dev_bin_counts, max_counts)
+                neur_ord = np.argsort(max_ind, kind='stable')
+                null_dev_dict_2[null_i].append(neur_ord)
+                
+        dev_seqs_array = np.array(dev_seqs) #num dev x num neur
+        for null_i in range(num_null):
+            null_dev_dict[null_i] = np.array(null_dev_dict[null_i]) #num dev x num neur x 2
+            null_dev_dict_2[null_i] = np.array(null_dev_dict_2[null_i]) #num dev x num neur x 2
+        toc = time.time()
+        print('\t\t\t\t\tTime to calculate sequences for ' + segment_names[s_i] + \
+              ' = ' + str(np.round((toc-tic)/60, 2)) + ' (min)')
+        np.save(os.path.join(sequence_dir,segment_names[s_i] + '_dev_seqs_array.npy'),dev_seqs_array,allow_pickle=True)
+        np.save(os.path.join(null_sequence_dir,segment_names[s_i] + '_null_dev_dict.npy'),null_dev_dict,allow_pickle=True)
+        np.save(os.path.join(null_sequence_dir_2,segment_names[s_i] + '_null_dev_dict.npy'),null_dev_dict_2,allow_pickle=True)
+        
     #Run rank order correlations for null data and true data and pull out 
     #significant events
     dev_rank_order_tests(dev_seqs_array, null_dev_dict, avg_taste_seqs_dict, 
@@ -1475,7 +1484,7 @@ def dev_rank_order_tests(dev_seqs_array, null_dev_dict, avg_taste_seqs_dict,
                     null_corrs[null_i,dev_i] = res[0]
             spearmans_dict[t_i][cp_i]['null'] = null_corrs
             #Since these are rank order corrs we'll absolute value them and calculate the 95th percentile for null
-            percentile_95 = np.percentile(null_corrs.flatten(),95)
+            percentile_95 = np.percentile(np.abs(null_corrs.flatten()),95)
             spearmans_dict[t_i][cp_i]['percentile_95'] = percentile_95
             sig_true_corrs = np.where(np.abs(true_corrs) > percentile_95)[0]
             spearmans_dict[t_i][cp_i]['sig_true_inds'] = sig_true_corrs
@@ -1571,6 +1580,7 @@ def dev_rank_order_tests(dev_seqs_array, null_dev_dict, avg_taste_seqs_dict,
     plt.tight_layout()
     f_taste_cdf.savefig(os.path.join(null_sequence_dir,seg_name+'_taste_compare_true_spearman_cdfs.png'))
     f_taste_cdf.savefig(os.path.join(null_sequence_dir,seg_name+'_taste_compare_true_spearman_cdfs.svg'))
+    plt.close(f_taste_cdf)
     
     #Epoch-v-epoch
     epoch_pairs = list(itertools.combinations(np.arange(max_num_cp),2))
@@ -1610,4 +1620,5 @@ def dev_rank_order_tests(dev_seqs_array, null_dev_dict, avg_taste_seqs_dict,
     plt.tight_layout()
     f_epoch_cdf.savefig(os.path.join(null_sequence_dir,seg_name+'_epoch_compare_true_spearman_cdfs.png'))
     f_epoch_cdf.savefig(os.path.join(null_sequence_dir,seg_name+'_epoch_compare_true_spearman_cdfs.svg'))
+    plt.close(f_epoch_cdf)
     